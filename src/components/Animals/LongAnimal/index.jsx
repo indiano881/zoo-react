6283fc005eed80e79f93339f884787afbf221b04
Davide Baldi <d.baldi88@gmail.com>
2024-03-31 09:57:10 +0200

    long animal fixed

--- src/components/Animals/LongAnimal/index.jsx
+++ src/components/Animals/LongAnimal/index.jsx
@@ -1,17 +1,18 @@
 import styles from './LongAnimal.module.css'
-
-const LongAnimal = ({name, lifespan, group, food, description, found, heigth, weight }) => { {/**smallImg */}
+import {getImageURL} from "../../../utils/functions"
+const LongAnimal = ({name, lifespan, group, food, description, found, heigth, weight, largeImg }) => { 
     return (
         <>
         <div className={styles.longAnimalContainer}>
-            <h2>{name}</h2>
-            <h4>{group}</h4>
-            <h4>{lifespan}</h4>
-            <h4>{food}</h4>
-            <h4>{found}</h4>
+        <img src={getImageURL(largeImg)} alt={name} height="450px" width="auto"/>
+            <h2>Name: {name}</h2>
+            <h4>Belogs to {group} group</h4>
+            <h4>Life expectancy is : {lifespan}</h4>
+            <h4>The {name} likes to eat {food}</h4>
+            <h4>Leaves in : {found}</h4>
+            <p> Height: {heigth}</p>
+            <p> Weight {weight}</p>
             <p>{description}</p>
-            <p>{heigth}</p>
-            <p>{weight}</p>
 
         </div>
           
