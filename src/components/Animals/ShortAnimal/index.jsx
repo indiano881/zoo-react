a9c9da5495ef5400283af132ffc31f4f732d56bc
Davide Baldi <d.baldi88@gmail.com>
2024-03-31 09:52:21 +0200

    read more button created

--- src/components/Animals/ShortAnimal/index.jsx
+++ src/components/Animals/ShortAnimal/index.jsx
@@ -1,7 +1,10 @@
 import styles from './ShortAnimal.module.css'
 import {getImageURL} from "../../../utils/functions"
 
-const ShortAnimal = ({name, group, food, description, found, smallImg }) => { {/**smallImg */}
+const ShortAnimal = ({name, group, food, description, found, smallImg }) => { 
+    const handleClick = () => {
+        
+    }
     return (
         <>
         <div className={styles.shortAnimalContainer}>
@@ -10,7 +13,7 @@ const ShortAnimal = ({name, group, food, description, found, smallImg }) => { {/
             <h4>{group}</h4>
             <h4>{food}</h4>
             <h4>{found}</h4>
-            <p>{description.slice(0,200)}</p>
+            <p>{description.slice(0,200)} <span className={styles.readMoreBtn}>...Read more</span></p>
 
         </div>
           
