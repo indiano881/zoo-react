99f0836cae32e1b261c0b19f7bb2bde652586833
Davide Baldi <d.baldi88@gmail.com>
2024-04-08 09:27:02 +0200

    cleaning up code

---
+++ src/components/Footer/FooterMessages/index.jsx
@@ -0,0 +1,10 @@
+const FooterMessages = ({openingHours,activity}) => {
+    return (
+        <>
+            <h4>{openingHours}</h4>
+            <h5>{activity}</h5>
+        </>
+    )
+}
+
+export default FooterMessages;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,57 +1,23 @@
 import styles from './Footer.module.css'
 import { day } from '../../utils/functions';
 import BuyTicket from '../BuyTicket';
+import FooterMessages from './FooterMessages';
 
 const Footer = () => {
     return (
         <>
           <footer className={styles.footer}>
             <BuyTicket />
-            {day===0 && 
-              <>
-              <h4>Sunday open: 10.00/18.00</h4>
-              <h5>Activity: feeding the Quokkas at 12.00</h5>
-              </>
-            }
-            {day===1 && 
-              <>
-              <h4>Mondays open: 11.00/16.00</h4>
-              <h5>10% Student discount all day</h5>
-              </>
-            }
-            {day===2 && 
-              <>
-              <h4>Tuesdays open: 11.00/16.00</h4>
-              <h5>20% Senior discount all day</h5>
-              </>
-            }
-            {day===3 && 
-              <>
-              <h4>Wendnesday open: 10.30/16.30</h4>
-              <h5>Activity: Swim with the Hawksbill turtle at 14.00</h5>
-              </>
-            }
-            {day===4 && 
-              <>
-              <h4>Thursday open: 10.00/17.30</h4>
-              <h5>Activity: play with the Tasmania Devil at 11.00</h5>
-              </>
-            }
-            {day===5 && 
-              <>
-              <h4>Fridays open: 10.00/20.30</h4>
-              <h5>Activity: pet and feed the Echidnas at 14.00</h5>
-              </>
-            }
-            {day===6 && 
-              <>
-              <h4>Saturdays open: 10.00/19</h4>
-              <h5>Activity: brunch at 11.00 with unlimited tiramisu´</h5>
-              </>
-            }
+            {day===0 && <FooterMessages openingHours="Sunday open: 10.00/18.00" activity="Activity: feeding the Quokkas at 12.00" />}
+            {day===1 && <FooterMessages openingHours="Mondays open: 11.00/16.00" activity="10% Student discount all day" />}
+            {day===2 && <FooterMessages openingHours="Tuesdays open: 11.00/16.00" activity="20% Senior discount all day" />}
+            {day===3 && <FooterMessages openingHours="Wendnesday open: 10.30/16.30" activity="Activity: Swim with the Hawksbill turtle at 14.00" />}
+            {day===4 && <FooterMessages openingHours="Thursday open: 10.00/17.30" activity="Activity: play with the Tasmania Devil at 11.00" />}
+            {day===5 && <FooterMessages openingHours="Fridays open: 10.00/20.30" activity="Activity: pet and feed the Echidnas at 14.00" />}
+            {day===6 && <FooterMessages openingHours="Saturdays open: 10.00/19" activity="Activity: brunch at 11.00 with unlimited tiramisu´" />}
           </footer>
         </>
     )
-};
+}
 
 export default Footer;
